2c7393848fa38ae5aafa3b80a27b829c31a0adbb
belloabraham <belloabraham101@gmail.com>
2023-10-11 07:32:03 +0100

    WIP

---
+++ angular/src/lib/transloco/transloco-root.module.ts
@@ -0,0 +1,20 @@
+import { provideTransloco, TranslocoModule } from '@ngneat/transloco';
+import { isDevMode, NgModule } from '@angular/core';
+import { TranslocoLoaderService } from './transloco-loader.service';
+import { Language } from '../i18n/language';
+
+@NgModule({
+  exports: [TranslocoModule],
+  providers: [
+    provideTransloco({
+      config: {
+        availableLangs: [Language.ENGLISH, Language.FRENCH],
+        defaultLang: Language.ENGLISH,
+        reRenderOnLangChange: true,
+        prodMode: isDevMode(),
+      },
+      loader: TranslocoLoaderService,
+    }),
+  ],
+})
+export class TranslocoRootModule {}
