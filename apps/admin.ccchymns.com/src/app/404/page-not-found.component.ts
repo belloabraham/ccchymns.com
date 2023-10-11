467b9630e4c5353e936fb4a234f59a1f6142750c
belloabraham <belloabraham101@gmail.com>
2023-10-11 08:43:11 +0100

    WIP

--- apps/admin.ccchymns.com/src/app/404/page-not-found.component.ts
+++ apps/admin.ccchymns.com/src/app/404/page-not-found.component.ts
@@ -1,6 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CCCHymnsPageNotFoundComponent } from '@ccchymns.com/common';
+import { CCCHymnsPageNotFoundComponent, LanguageResourceKey } from '@ccchymns.com/common';
+import { ILanguageResourceService, LANGUAGE_RESOURCE_TOKEN } from '@ccchymns.com/angular';
+import { Title } from '@angular/platform-browser';
+import { SubSink } from 'subsink';
+import { Store } from '@ngrx/store';
+import { getLanguageLoadedSelector } from '../../store/selectors/language-resource.selector';
+
 
 @Component({
   selector: 'ccchymns-page-not-found',
@@ -10,4 +16,31 @@ import { CCCHymnsPageNotFoundComponent } from '@ccchymns.com/common';
   styleUrls: ['./page-not-found.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PageNotFoundComponent {}
+export class PageNotFoundComponent implements OnInit, OnDestroy {
+  private subscriptions = new SubSink();
+
+  constructor(
+    private ngrxStore: Store,
+    @Inject(LANGUAGE_RESOURCE_TOKEN)
+    private languageResourceService: ILanguageResourceService,
+    private title: Title
+  ) {}
+
+  ngOnInit(): void {
+    this.onLanguageResourceLoad();
+  }
+
+  onLanguageResourceLoad() {
+    this.subscriptions.sink = this.ngrxStore
+      .select(getLanguageLoadedSelector())
+      .subscribe(() => {
+        const pageTitle = this.languageResourceService.getString(
+          LanguageResourceKey.PAGE_TITLE
+        );
+        this.title.setTitle(pageTitle);
+      });
+  }
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+}
