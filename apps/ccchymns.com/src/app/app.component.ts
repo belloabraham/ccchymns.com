ceae5bbe1bab297b9164610ff1863c4f881cdb03
Ridwan Bello <ridwan.bello@seech.com>
2023-10-11 08:57:25 +0100

    WIP

--- apps/ccchymns.com/src/app/app.component.ts
+++ apps/ccchymns.com/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ILanguageResourceService, LANGUAGE_RESOURCE_TOKEN, Language } from '@ccchymns.com/angular';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'ccchymns-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ccchymns.com';
+export class AppComponent implements OnInit, OnDestroy {
+  private subscriptions = new SubSink();
+
+  constructor(
+    @Inject(LANGUAGE_RESOURCE_TOKEN)
+    private languageResourceService: ILanguageResourceService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loadLanguageResource(Language.ENGLISH, () => {
+      this.onLanguageLoaded();
+    });
+  }
+
+  private loadLanguageResource(
+    language: string,
+    onLanguageResourceLoaded: () => void
+  ) {
+    this.subscriptions.sink = this.languageResourceService
+      .loadLanguageResource(language)
+      .subscribe(() => {
+        onLanguageResourceLoaded();
+      });
+  }
+
+  onLanguageLoaded() {
+    this.languageResourceService.setLanguageResourceLoadedSuccessfully(true);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
