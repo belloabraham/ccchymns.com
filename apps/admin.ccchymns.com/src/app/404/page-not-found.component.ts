85d0941ef4c1387f94c52d90cf04e3463c871f9e
belloabraham <belloabraham101@gmail.com>
2023-11-15 05:51:23 +0100

    Merged all storybook to UI

--- apps/admin.ccchymns.com/src/app/404/page-not-found.component.ts
+++ apps/admin.ccchymns.com/src/app/404/page-not-found.component.ts
@@ -24,7 +24,6 @@ import { Title } from '@angular/platform-browser';
   standalone: true,
   imports: [CommonModule, CCCPageNotFoundComponent],
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PageNotFoundComponent implements OnInit, OnDestroy {
