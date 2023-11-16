7a0e3ced439fda6f841f38f3ab386858f1f8df33
belloabraham <belloabraham101@gmail.com>
2023-11-16 18:13:12 +0100

    WIP

--- apps/admin.ccchymns.com/src/app/dashboard/lyrics/yoruba/yoruba.component.ts
+++ apps/admin.ccchymns.com/src/app/dashboard/lyrics/yoruba/yoruba.component.ts
@@ -1,14 +1,41 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { CCCIconDirective } from '@ccchymns.com/ui';
-import { NgMaterialButtonModule } from '@ccchymns.com/angular';
+import { NgMatTooltipModule, NgMaterialButtonModule } from '@ccchymns.com/angular';
+import { SubSink } from 'subsink';
+import { DisplayService, Size } from '@ccchymns.com/common';
 
 @Component({
   selector: 'app-lyrics-yoruba',
   standalone: true,
-  imports: [SharedModule, CCCIconDirective, NgMaterialButtonModule],
+  imports: [
+    SharedModule,
+    CCCIconDirective,
+    NgMaterialButtonModule,
+    NgMatTooltipModule,
+  ],
   templateUrl: './yoruba.component.html',
   styleUrl: './yoruba.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class YorubaComponent {}
+export class YorubaComponent implements OnDestroy {
+  loopArray = new Array(70);
+  private subscriptions = new SubSink();
+  isDesktop = false;
+
+  constructor(private displayService: DisplayService) {
+    this.getIsDeviceDisplayDesktopAsync();
+  }
+
+  getIsDeviceDisplayDesktopAsync() {
+    this.subscriptions.sink = this.displayService.size$.subscribe(
+      (displaySize) => {
+        this.isDesktop = displaySize === Size.Large;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+}
