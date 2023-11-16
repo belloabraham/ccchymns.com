092ad38ebd12fd4c66cf81c032af90825e4cc350
belloabraham <belloabraham101@gmail.com>
2023-11-16 07:53:08 +0100

    .

--- apps/ccchymns.com/src/app/faq/faq.component.ts
+++ apps/ccchymns.com/src/app/faq/faq.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'ccchymns.com-faq',
+  selector: 'app-faq',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './faq.component.html',
