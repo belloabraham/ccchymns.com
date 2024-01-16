54353ed12044adf8efc0cf5a695f9fdeb37abc68
belloabraham <belloabraham101@gmail.com>
2024-01-16 07:50:06 +0100

    WIP

--- apps/admin.ccchymns.com/src/app/dashboard/tonic-solfa/i18n/language-resource-key.ts
+++ apps/admin.ccchymns.com/src/app/dashboard/tonic-solfa/i18n/language-resource-key.ts
@@ -1,4 +1,5 @@
 export class LanguageResourceKey {
   static readonly EMPTY_STATE_DESCRIPTION = 'all_hymns_empty_state_description';
-  static readonly ADD_TONIC_SOLFA='add_tonic_solfa'
+  static readonly ADD_TONIC_SOLFA = 'add_tonic_solfa';
+  static readonly SEARCH_TONIC_SOLFA_MSG = 'search_tonic_solfa';
 }
--- apps/admin.ccchymns.com/src/app/dashboard/tonic-solfa/tonic-solfa-placeholder/tonic-solfa-placeholder.component.ts
+++ apps/admin.ccchymns.com/src/app/dashboard/tonic-solfa/tonic-solfa-placeholder/tonic-solfa-placeholder.component.ts
@@ -1,13 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { TABLE_PAGE_SIZE } from '../../shared';
+import { NgFor } from '@angular/common';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-tonic-solfa-placeholder',
   standalone: true,
-  imports: [],
+  imports: [TranslocoModule, NgFor],
   templateUrl: './tonic-solfa-placeholder.component.html',
   styleUrl: './tonic-solfa-placeholder.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TonicSolfaPlaceholderComponent {
-
+  placeholders = Array(TABLE_PAGE_SIZE / 2);
+  @Input({ required: true }) columnNames: string[] = [];
 }
