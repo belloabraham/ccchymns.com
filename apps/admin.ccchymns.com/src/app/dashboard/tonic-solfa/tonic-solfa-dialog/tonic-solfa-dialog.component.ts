d58d8deb2e5b8b07bc6b80e989c8476179504709
belloabraham <belloabraham101@gmail.com>
2024-03-05 08:12:05 +0100

    WIP

--- apps/admin.ccchymns.com/src/app/dashboard/tonic-solfa/tonic-solfa-dialog/tonic-solfa-dialog.component.ts
+++ apps/admin.ccchymns.com/src/app/dashboard/tonic-solfa/tonic-solfa-dialog/tonic-solfa-dialog.component.ts
@@ -6,13 +6,32 @@ import {
 } from '@angular/core';
 import { NgMaterialButtonModule } from '@ccchymns.com/angular';
 import { SharedModule } from '../../shared';
-import { RootLanguageResourceKey } from '@ccchymns.com/common';
+import {
+  DisplayService,
+  IEditorsTonicSolfa,
+  RootLanguageResourceKey,
+} from '@ccchymns.com/common';
 import { DashboardLanguageResourceKey } from '../../i18n/language-resource-key';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ITonicSolfaForm } from '../form';
 import { LanguageResourceKey } from '../i18n/language-resource-key';
 import { TuiDialogContext } from '@taiga-ui/core';
 import { POLYMORPHEUS_CONTEXT } from '@tinkoff/ng-polymorpheus';
+import { StorageService } from '../../storage.service';
+import { Router } from '@angular/router';
+import { TonicSolfaDataService } from '../tonic-solfa.data.service';
+import {
+  FileUtil,
+  LoggerUtil,
+  NotificationBuilder,
+  Shield,
+} from '@ccchymns.com/core';
+import {
+  StoragePath,
+  genericRetryStrategy,
+} from 'apps/admin.ccchymns.com/src/core';
+import { SubSink } from 'subsink';
+import { from, retryWhen } from 'rxjs';
 
 @Component({
   selector: 'app-tonic-solfa-dialog',
@@ -35,6 +54,15 @@ export class TonicSolfaDialogComponent implements OnInit {
   fileNameFC = new FormControl<string | null>(null, [Validators.required]);
   formSubmitted = false;
   file?: File;
+  private subscriptions = new SubSink();
+
+  constructor(
+    @Inject(POLYMORPHEUS_CONTEXT)
+    readonly context: TuiDialogContext<void>,
+    private tonicSolfaDataService: TonicSolfaDataService,
+    private storageService: StorageService,
+    private displayService: DisplayService
+  ) {}
 
   hymnNoIsInvalid() {
     return this.formSubmitted && this.hymnNoFC.invalid;
@@ -62,16 +90,77 @@ export class TonicSolfaDialogComponent implements OnInit {
     }
   }
 
-  constructor(
-    @Inject(POLYMORPHEUS_CONTEXT)
-    readonly context: TuiDialogContext<void>
-  ) {}
-
   onSubmit() {
     this.formSubmitted = true;
     this.context.$implicit.complete();
     if (this.tonicSolfaForm.valid) {
-      //TODO
+      const no = this.hymnNoFC.value!;
+      const file = this.file!;
+      const fileNameWithExt = no + '.' + FileUtil.EXTENSION.PDF;
+      const pdfFile = FileUtil.rename(file, fileNameWithExt);
+      const responsiveSvgSize = this.displayService.percentage * 60;
+      const responsiveFontSize = this.displayService.percentage * 16;
+      Shield.pulse(
+        responsiveFontSize,
+        responsiveSvgSize,
+        `Uploading Tonic Solfa please wait`
+      );
+      const storagePath = [StoragePath.TONIC_SOILFA, fileNameWithExt];
+      this.uploadEditorsTonicSolfa(no, storagePath, pdfFile);
     }
+  }
+
+  private uploadEditorsTonicSolfa(
+    no: number,
+    storagePath: string[],
+    pdfFile: File
+  ) {
+    this.storageService.uploadFile(storagePath, pdfFile, {
+      onComplete: (fileDownloadUrl) => {
+        this.subscriptions.sink = from(
+          this.updateEditorsUploadedTonicSolfaRecord(no, fileDownloadUrl)
+        )
+          .pipe(retryWhen(genericRetryStrategy()))
+          .subscribe({
+            next: () => {
+              new NotificationBuilder()
+                .build()
+                .success('Tonic solfa uploaded successfully');
+              this.context.$implicit.complete();
+              Shield.remove();
+            },
+            error: (error) => {
+              this.storageService.deleteFileFrom(storagePath);
+              this.showUploadFailedNotification(error);
+              Shield.remove();
+              LoggerUtil.error(this, this.uploadEditorsTonicSolfa.name, error);
+            },
+          });
+      },
+      onError: (error) => {
+        this.showUploadFailedNotification(error);
+        LoggerUtil.error(this, this.uploadEditorsTonicSolfa.name, error);
+        Shield.remove();
+      },
+    });
+  }
+
+  private updateEditorsUploadedTonicSolfaRecord(
+    no: number,
+    downloadUrl: string
+  ) {
+    const data: IEditorsTonicSolfa = {
+      no: no,
+      url: downloadUrl,
+    };
+
+    return this.tonicSolfaDataService.updateTonicSolfa(data);
+  }
+
+  private showUploadFailedNotification(error: any) {
+    new NotificationBuilder().build()
+      .error(`Unable to upload tonic solfa at the moment, try again later.
+      Error: ${error}
+    `);
   }
 }
--- apps/admin.ccchymns.com/src/core/storage/path.ts
+++ apps/admin.ccchymns.com/src/core/storage/path.ts
@@ -1,5 +1,6 @@
 export class StoragePath {
   static readonly AUDIO = 'audio';
+  static readonly TONIC_SOILFA = 'tonic_solfa';
   static readonly EGUN = 'egun';
   static readonly FRENCH = 'french';
   static readonly YORUBA = 'yoruba';
