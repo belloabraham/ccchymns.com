b8c39de3e7d1d309385088eef3f6c80a5d77f8dd
belloabraham <belloabraham101@gmail.com>
2024-03-14 18:27:27 +0100

    WIP

--- apps/admin.ccchymns.com/src/app/dashboard/all-hymns/datasource/all-hymns-datasource.ts
+++ apps/admin.ccchymns.com/src/app/dashboard/all-hymns/datasource/all-hymns-datasource.ts
@@ -5,4 +5,20 @@ export class AllHymnsDataSource extends BaseDataSource<IAllHymns> {
   constructor(data: IAllHymns[]) {
     super(data);
   }
+
+  override filterTableData(filterBy?: string | undefined): void {
+      if (filterBy) {
+        const filterValue = filterBy.toLowerCase();
+        this.filteredData = this.data.filter((item) => {
+          const stringToSearch = `${item.lyric?.no}`;
+          return stringToSearch.includes(filterValue);
+        });
+        this.data$.next(this.filteredData);
+      }
+
+      if (!filterBy) {
+        this.filteredData = [];
+        this.data$.next(this.paginatedData);
+      }
+  }
 }
